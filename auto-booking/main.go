2a9bf7849e1e0398e0af1448b3252e9a649e4d69
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-11-14 00:08:24 +0900

    auto-booking: add sample of chromedp

--- auto-booking/main.go
+++ auto-booking/main.go
@@ -15,11 +15,58 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
 
 	"github.com/knq/chromedp"
+	"github.com/knq/chromedp/cdp"
 )
 
 func main() {
-	fmt.Println("hello")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c, err := chromedp.New(ctx, chromedp.WithLog(log.Printf))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var site, res string
+	err = c.Run(ctx, googleSearch("site:brank.as", "Home", &site, &res))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = c.Shutdown(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = c.Wait()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("saved screenshot from search result listing `%s` (%s)\n", res, site)
+}
+
+func googleSearch(q, text string, site, res *string) chromedp.Tasks {
+	var buf []byte
+	sel := fmt.Sprintf(`//a[text()[contains(., '%s')]]`, text)
+	return chromedp.Tasks{
+		chromedp.Navigate(`https://www.google.com/`),
+		chromedp.WaitVisible(`#hplogo`, chromedp.ByID),
+		chromedp.SendKeys(`#lst-ib`, q+"\n", chromedp.ByID),
+		chromedp.WaitVisible(`#res`, chromedp.ByID),
+		chromedp.Text(sel, res),
+		chromedp.Click(sel),
+		chromedp.WaitVisible(`a[href="/brankas-for-business"]`, chromedp.ByQuery),
+		chromedp.WaitNotVisible(`.preloader-content`, chromedp.ByQuery),
+		chromedp.Location(site),
+		chromedp.ScrollIntoView(`.banner-section.third-section`, chromedp.ByQuery),
+		chromedp.Sleep(2 * time.Second), // wait for animation to finish
+		chromedp.Screenshot(`.banner-section.third-section`, &buf, chromedp.ByQuery),
+		chromedp.ActionFunc(func(context.Context, cdp.Handler) error {
+			return ioutil.WriteFile("screenshot.png", buf, 0644)
+		}),
+	}
 }
