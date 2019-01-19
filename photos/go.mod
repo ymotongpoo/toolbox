85cb144f671685d3db159b00b35f39f98aa43c61
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2019-01-19 23:05:16 +0900

    photos: first working version of drive to photos uploader

--- photos/go.mod
+++ photos/go.mod
@@ -1,6 +1,17 @@
 module github.com/ymotongpoo/toolbox/photos
 
 require (
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/nmrshll/google-photos-api-client-go v0.0.0-20180906090521-a94d2fbcfb40
+	github.com/nmrshll/oauth2-noserver v0.0.0-20180827223500-16b622b98a45 // indirect
+	github.com/nmrshll/rndm-go v0.0.0-20170430161430-8da3024e53de // indirect
+	github.com/palantir/stacktrace v0.0.0-20161112013806-78658fd2d177 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.3.0
+	github.com/skratchdot/open-golang v0.0.0-20190104022628-a2dfa6d0dab6 // indirect
 	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890
 	google.golang.org/api v0.1.0
 )
--- photos/main.go
+++ photos/main.go
@@ -19,6 +19,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -27,73 +28,196 @@ import (
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 
+	photoslib "github.com/nmrshll/google-photos-api-client-go/lib-gphotos"
+	photoscli "github.com/nmrshll/google-photos-api-client-go/noserver-gphotos"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	drive "google.golang.org/api/drive/v3"
+	photoslibrary "google.golang.org/api/photoslibrary/v1"
 )
 
 const (
-	DefaultGoogleDriveSecretsFile  = "drive_client_secret.json"
-	DefualtGooglePhotosSecretsFile = "photos_client_secret.json"
+	DefaultSecretsFile = "credentials.json"
 )
 
-type Drive struct {
-	secrets string
-	service *drive.Service
-}
+var (
+	sourceFolderID string
+	secretsFile    string
+	targetAlbumID  string
+	logger         *logrus.Logger
+)
 
-func (d *Drive) Init(secrets string) error {
-	ctx := context.Background()
-	d.secrets = secrets
-	b, err := ioutil.ReadFile(secrets)
-	if err != nil {
-		return err
-	}
-	config, err := google.ConfigFromJSON(b, drive.DriveScope)
-	if err != nil {
-		return err
-	}
-	client, err := getHTTPClient(ctx, config)
-	if err != nil {
-		return err
-	}
-	service, err := drive.New(client)
-	if err != nil {
-		return err
-	}
-	d.service = service
-	return nil
-}
-
-func (d *Drive) FetchFileList(id string) ([]drive.File, error) {
-	files := []drive.File{}
-	query := fmt.Sprintf("'%s' in parents", id)
-	fl, err := d.service.Files.List().Q(query).Do()
-	if err != nil {
-		return nil, err
-	}
-	files = fl.Files
-	return files, nil
+func init() {
+	flag.StringVar(&sourceFolderID, "source", "", "Source Google Drive Folder ID")
+	flag.StringVar(&secretsFile, "ds", DefaultSecretsFile, "Path to credential JSON file")
+	flag.StringVar(&targetAlbumID, "target", "", "Target Google Photos album ID")
+	logger = logrus.StandardLogger()
 }
 
 func main() {
 	flag.Parse()
+	c, err := NewClient(secretsFile)
+	if err != nil {
+		logger.Fatalf("error creating drive instance: %v", err)
+	}
+	files, err := c.FetchDriveFileList(sourceFolderID)
+	if err != nil {
+		logger.Fatalf("error fetching file: %v", err)
+	}
+	for _, f := range files {
+		prodessCopy(c, f)
+	}
+}
+
+func prodessCopy(c *Client, f *drive.File) error {
+	logger.Infof("[download]: %s (%s)", f.Name, f.Id)
+	n, name, err := c.DownloadFile(f)
+	if err != nil {
+		logger.Errorf("[main] failed to download %s: %v", f.Name, err)
+		return err
+	}
+	logger.Infof("[download]: done (%d bytes)", n)
+	logger.Infof("[upload]: %v", name)
+	_, err = c.UploadToPhotos(name)
+	if err != nil {
+		logger.Errorf("[main] failed to upload %s: %v", name, err)
+		return err
+	}
+	logger.Infof("[upload]: done upload %s", name)
+	logger.Infof("[clean up]: removing file %v", name)
+	err = os.Remove(name)
+	if err != nil {
+		logger.Errorf("[main] failed to remove %s: %v", name, err)
+		return err
+	}
+	logger.Infof("[clean up]: done")
+	return nil
+}
+
+// ---- Google services ----
+
+type Client struct {
+	secrets   string
+	driveSrv  *drive.Service
+	photosSrv *photoscli.Client
+}
+
+func NewClient(secrets string) (*Client, error) {
+	ctx := context.Background()
+	c := Client{
+		secrets: secrets,
+	}
+	b, err := ioutil.ReadFile(secrets)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewClient")
+	}
+	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewClient")
+	}
+	client, err := getHTTPClient(ctx, config)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewClient")
+	}
+	driveSrv, err := drive.New(client)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewClient")
+	}
+	c.driveSrv = driveSrv
+
+	photosClient, err := photoscli.NewClient(
+		photoscli.AuthenticateUser(
+			photoslib.NewOAuthConfig(
+				photoslib.APIAppCredentials{
+					ClientID:     config.ClientID,
+					ClientSecret: config.ClientSecret,
+				},
+			),
+		),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewClient: Photos")
+	}
+	c.photosSrv = photosClient
+	return &c, nil
+}
+
+func (c *Client) FetchDriveFileList(id string) ([]*drive.File, error) {
+	files := []*drive.File{}
+	query := fmt.Sprintf("'%s' in parents", id)
+	fl, err := c.driveSrv.Files.List().
+		Fields("nextPageToken, files(id, name, kind, mimeType)").
+		Q(query).
+		Do()
+	if err != nil {
+		return nil, errors.Wrap(err, "FetchFileList: first")
+	}
+	files = append(files, fl.Files...)
+	logger.Infof("FetchFileList: %d files", len(files))
+	for fl.NextPageToken != "" {
+		fln, err := c.driveSrv.Files.List().
+			Fields("nextPageToken, files(id, name, kind, mimeType)").
+			Q(query).
+			PageToken(fl.NextPageToken).
+			Do()
+		if err != nil {
+			return nil, errors.Wrap(err, "FetchFileList: loop")
+		}
+		files = append(files, fl.Files...)
+		logger.Infof("FetchFileList: %d files", len(files))
+		fl = fln
+	}
+	return files, nil
+}
+
+func (c *Client) DownloadFile(f *drive.File) (int64, string, error) {
+	file, err := os.Create(f.Name)
+	if err != nil {
+		logger.Errorf("failed to create file: %s", f.Name)
+		return 0, "", errors.Wrap(err, "DownloadFile: create file")
+	}
+	defer file.Close()
+
+	res, err := c.driveSrv.Files.Get(f.Id).Download()
+	if err != nil {
+		return 0, "", errors.Wrap(err, "DownloadFile: Download()")
+	}
+	defer res.Body.Close()
+	n, err := io.Copy(file, res.Body)
+	if err != nil {
+		return 0, "", errors.Wrap(err, "DownloadFile: copy data")
+	}
+	return n, f.Name, nil
+}
+
+func (c *Client) UploadToPhotos(name string) (*photoslibrary.MediaItem, error) {
+	return c.photosSrv.UploadFile(name)
 }
 
 // ---- OAuth2 ----
 
 func getHTTPClient(ctx context.Context, config *oauth2.Config) (*http.Client, error) {
 	tokenCacheDir := ".credentials"
-	err := os.MkdirAll(tokenCacheDir, 0755)
+	tokenCacheFile := filepath.Join(tokenCacheDir, "cache.json")
+	_, err := os.Open(tokenCacheFile)
 	if err != nil {
-		return nil, err
+		err := os.MkdirAll(tokenCacheDir, 0755)
+		if err != nil {
+			return nil, errors.Wrap(err, "getHTTPClient")
+		}
+		_, err = os.Create(tokenCacheFile)
+		if err != nil {
+			return nil, errors.Wrap(err, "getHTTPClient")
+		}
 	}
-	cache := filepath.Join(tokenCacheDir, "cache.json")
-	tok, err := getToken(cache, config)
+
+	tok, err := getToken(tokenCacheFile, config)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "getHTTPClient")
 	}
-	err = saveToken(cache, tok)
+	err = saveToken(tokenCacheFile, tok)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "getHTTPClient")
 	}
 	return config.Client(ctx, tok), nil
 }
@@ -125,7 +249,7 @@ func getToken(cache string, config *oauth2.Config) (*oauth2.Token, error) {
 }
 
 func saveToken(path string, token *oauth2.Token) error {
-	fmt.Printf("Saving credential to %s\n", path)
+	logger.Infof("Saving credential to %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		return err
